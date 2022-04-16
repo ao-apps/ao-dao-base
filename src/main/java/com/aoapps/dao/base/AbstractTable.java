52cddf7ed86aa2c082222ca02e46cfa2d0f2ee25
AO Industries, Inc <support@aoindustries.com>
2022-04-16 01:49:24 -0500

    Added blank line before package declarations
    Resolves "EmptyLineSeparator: 'package' should be separated from previous line."

--- src/main/java/com/aoapps/dao/base/AbstractTable.java
+++ src/main/java/com/aoapps/dao/base/AbstractTable.java
@@ -1,6 +1,6 @@
 /*
  * ao-dao-base - Simple data access objects framework base for implementations.
- * Copyright (C) 2011, 2012, 2013, 2015, 2016, 2020, 2021  AO Industries, Inc.
+ * Copyright (C) 2011, 2012, 2013, 2015, 2016, 2020, 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-dao-base.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.dao.base;
 
 import com.aoapps.dao.Model;
--- src/main/java/com/aoapps/dao/base/Encryption.java
+++ src/main/java/com/aoapps/dao/base/Encryption.java
@@ -20,6 +20,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with ao-dao-base.  If not, see <https://www.gnu.org/licenses/>.
  */
+
 package com.aoapps.dao.base;
 
 import com.aoapps.lang.exception.WrappedException;
