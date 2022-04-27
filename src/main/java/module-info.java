44f74df3a937595e47afd96cf264c1800d1bf6b4
AO Industries, Inc <support@aoindustries.com>
2022-04-26 23:57:26 -0500

    Activated org.openrewrite.java.cleanup.Cleanup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -29,4 +29,4 @@ module com.aoapps.dao.base {
   requires static com.aoapps.security; // <groupId>com.aoapps</groupId><artifactId>ao-security</artifactId>
   // Java SE
   requires java.sql;
-}
+} // TODO: Avoiding rewrite-maven-plugin-4.22.2 truncation
